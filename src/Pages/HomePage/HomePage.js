385271be56e966edb70dfe9313960dc4e14d58bc
Daniele Scotti <daniele@dueper.net>
2019-06-18 21:49:55 +0200

    update HomePage content

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link } from 'react-router-dom';
+import cocktails from './cocktails.svg';
 import style from './HomePage.module.scss';
 
 function HomePage() {
   return (
     <div className={style.wrapper}>
       <header className={style.header}>
-        <img src={logo} className={style.logo} alt="logo" />
+        <img src={cocktails} className={style.logo} alt="logo" />
+        <h1>React Cocktail</h1>
         <p>
-          Edit <code>src/HomePage.js</code> and save to reload.
+          Find the cocktail you like and learn its recipe.
         </p>
-        <a
+        <Link
           className={style.link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          to="/search"
         >
-          Learn React
-        </a>
+          Search Cocktail
+        </Link>
       </header>
     </div>
   );
